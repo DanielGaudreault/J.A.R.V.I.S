ceadd0df9026622538aa73999f4cce97b783becf
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2024-05-13 19:26:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ function sendCommand() {
     // Process userInput and generate response (in this case, just echoing back the command)
     // You can add more sophisticated logic here
 }
+
+// Add event listener to execute sendCommand function when the button is clicked
+document.getElementById("sendButton").addEventListener("click", sendCommand);
