f06ff83ee3240f54a3e923599a031e4ca7c9962e
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2024-05-13 19:25:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 function sendCommand() {
     var userInput = document.getElementById("userInput").value;
     document.getElementById("response").innerHTML = "Command: " + userInput;
-
-    // Send userInput to backend for processing (AJAX or fetch)
-    // Example:
-    // fetch('/process', {
-    //     method: 'POST',
-    //     body: JSON.stringify({ command: userInput }),
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    // })
-    // .then(response => response.json())
-    // .then(data => {
-    //     document.getElementById("response").innerHTML = "Response: " + data.response;
-    // })
-    // .catch(error => {
-    //     console.error('Error:', error);
-    // });
+    // Process userInput and generate response (in this case, just echoing back the command)
+    // You can add more sophisticated logic here
 }
