3ab0e9684d66379b4ecb3ea76f4478b8948d51d4
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2024-05-13 22:40:54 -0400

    Delete calendarService.js

--- calendarService.js
+++
@@ -1,5 +0,0 @@
-// calendarService.js
-
-// Example code to interact with a calendar service API
-// Implement functions to add, update, delete, and retrieve calendar events
-// This code will depend on the specific calendar service API you're using
