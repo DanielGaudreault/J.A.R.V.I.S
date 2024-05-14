286dc5f9d8dafebdb94832b7bcccdc0ec46ff1e8
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2024-05-13 22:41:09 -0400

    Delete emailService.js

--- emailService.js
+++
@@ -1,5 +0,0 @@
-// emailService.js
-
-// Example code to interact with an email service provider API
-// Implement functions to send emails with attachments, HTML content, etc.
-// This code will depend on the specific email service provider API you're using
